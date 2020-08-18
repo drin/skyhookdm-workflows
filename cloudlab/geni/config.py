f3d74c35279164274ae2edb5482a8269768ff123
Aldrin <akmontan@ucsc.edu>
2020-08-18 09:35:27 -0500

    geni/config.py: Arbitrary code style change

--- cloudlab/geni/config.py
+++ cloudlab/geni/config.py
@@ -1,7 +1,7 @@
 import cloudlab_cmd
 
 # name of experiment to use to identify this allocation
-experiment_name = 'popper-ceph'
+experiment_name = 'singlecell-test'
 
 # expiration of allocation in minutes
 expiration = 180
@@ -10,36 +10,39 @@ expiration = 180
 img = "urn:publicid:IDN+clemson.cloudlab.us+image+schedock-PG0:ubuntu18-docker"
 
 # check availability and types at https://www.cloudlab.us/resinfo.php
-site = 'clemson'
+site    = 'clemson'
 hw_type = 'c6320'
 
 # create an internal network
 with_lan = True
 
-############
-# multi-node
-############
-# grouping of nodes based on their ceph roles
-#num_osds = 3
-#groups = {
-#   'mons': ['mon1'],
-#   'mgrs': ['mon1'],
-#   'osds': ['osd{}'.format(n) for n in range(1, num_osds+1)],
-#}
-
-
-#############
+# ------------------------------
 # single node
-#############
-groups = {
+num_osds = 1
+groups   = {
     'mons': ['onlynode'],
     'osds': ['onlynode'],
     'mgrs': ['onlynode'],
 }
 
+# ------------------------------
+# multi-node
+# num_osds = 3
+# groups   = {
+#    'mons': ['mon1'],
+#    'mgrs': ['mon1'],
+#    'osds': [f'osd{n}' for n in range(1, num_osds+1)],
+# }
 
-#############
+
+# ------------------------------
 # run command (parse 'apply', 'destroy', 'get-inventory' and 'renew'
-#############
-cloudlab_cmd.run(experiment_name, expiration, site, groups, hw_type, img,
-                 with_lan)
+cloudlab_cmd.run(
+    experiment_name,
+    expiration,
+    site,
+    groups,
+    hw_type,
+    img,
+    with_lan
+)
