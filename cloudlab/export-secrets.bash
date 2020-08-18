7cb51eaf08724b0ad942f8e86917f66e116102ad
akmontan <akmontan@client0.singlecell-test.skyhook-pg0.wisc.cloudlab.us>
2020-08-18 09:34:31 -0500

    Adding a script that shows secrets to export

---
+++ cloudlab/export-secrets.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export GENI_FRAMEWORK="emulab-ch2"
+export GENI_PROJECT="skytether-singlecell"
+export GENI_USERNAME="akmontan"
+export GENI_KEY_PASSPHRASE=
+export GENI_PUBKEY_DATA=
+export GENI_CERT_DATA=
+export ANSIBLE_SSH_KEY_DATA=
+
+echo ${GENI_FRAMEWORK}
